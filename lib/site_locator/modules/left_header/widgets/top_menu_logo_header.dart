e6c921deab60fb8a4fe61e10719cd8e90f4a7edd
Anubhav Sagar <Anubhav.Sagar@fleetcor.com>
2024-02-19 12:51:17 +0530

    UCX-23366 - Add config check on UI

--- lib/site_locator/modules/left_header/widgets/top_menu_logo_header.dart
+++ lib/site_locator/modules/left_header/widgets/top_menu_logo_header.dart
@@ -1,3 +1,4 @@
+import 'package:driven_site_locator/data/model/entitlement_repository.dart';
 import 'package:driven_site_locator/site_locator/modules/left_header/widgets/fuelman_network_sitelocator_logo.dart';
 import 'package:driven_site_locator/site_locator/modules/left_header/widgets/hamberger_menu.dart';
 import 'package:flutter/material.dart';
@@ -5,9 +6,13 @@ import 'package:flutter/material.dart';
 class TopMenuLogoHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _entitlementRepository = SiteLocatorEntitlementUtils.instance;
     return Row(
       children: [
-        HambergerMenu(),
+        Visibility(
+          visible: _entitlementRepository.isDisplaySettingsEnabled,
+          child: HambergerMenu(),
+        ),
         FuelmanNetworkSitelocatorLogo(),
       ],
     );
