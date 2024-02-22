fa9d4e0a526cdfd62ee8b8f8d98ffa18081640a9
SmeetBhatt <smeet.bhatt@3frameslab.com>
2024-02-22 18:46:24 +0530

    Add left header

---
+++ lib/site_locator/modules/left_header/widgets/fuelman_network_sitelocator_logo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FuelmanNetworkSitelocatorLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      width: 255,
+      child: Image.asset(
+        'assets/images/fuelman_network_sitelocator_logo.png',
+      ),
+    );
+  }
+}
