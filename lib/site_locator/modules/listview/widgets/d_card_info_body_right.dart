cb98d171cae96b499dab133cca98f29159b01e0f
SnehaMudhigonda <sneha.mudhigonda@fleetcor.com>
2024-02-27 01:36:18 +0530

    alignment issue

--- lib/site_locator/modules/listview/widgets/d_card_info_body_right.dart
+++ lib/site_locator/modules/listview/widgets/d_card_info_body_right.dart
@@ -10,17 +10,14 @@ class CardInfoBodyRight extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.4,
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CardInfoFuelPriceFork(siteLocation),
-            const SizedBox(height: 18),
-            _favoriteSection(),
-            _serviceOrTime(),
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CardInfoFuelPriceFork(siteLocation),
+          const SizedBox(height: 18),
+          _favoriteSection(),
+          _serviceOrTime(),
+        ],
       ),
     );
   }
