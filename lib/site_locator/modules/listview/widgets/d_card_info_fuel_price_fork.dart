ccc307fa62edba6304b688f5f6373ef9843114bb
SnehaMudhigonda <sneha.mudhigonda@fleetcor.com>
2024-02-27 02:11:22 +0530

    text align

--- lib/site_locator/modules/listview/widgets/d_card_info_fuel_price_fork.dart
+++ lib/site_locator/modules/listview/widgets/d_card_info_fuel_price_fork.dart
@@ -32,11 +32,8 @@ class CardInfoFuelPriceFork extends StatelessWidget {
   }
 
   Widget _displayPriceItem(String displayText) {
-    final double topPadding = SiteInfoUtils.canDisplayBrandLogo(siteLocation)
-        ? kIsWeb
-            ? 0
-            : 7
-        : 0;
+    final double topPadding =
+        SiteInfoUtils.canDisplayBrandLogo(siteLocation) ? 0 : 0;
     return Container(
       margin: EdgeInsets.only(top: topPadding),
       child: Semantics(
