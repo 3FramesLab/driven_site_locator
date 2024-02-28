0846d5d9a99af7b37827e2a7b7a88e8a672bc45d
SivaGandrikala <siva.gandrikala@fleetcor.com>
2024-02-28 11:41:50 +0530

    handle tab navigation

--- lib/site_locator/modules/left_header/web_menu/web_menu_items/web_help_center_card.dart
+++ lib/site_locator/modules/left_header/web_menu/web_menu_items/web_help_center_card.dart
@@ -1,8 +1,8 @@
-import 'package:driven_site_locator/config/site_locator_navigation.dart';
 import 'package:driven_site_locator/site_locator/configuration/site_locator_config.dart';
 import 'package:driven_site_locator/site_locator/constants/site_locator_constants.dart';
 import 'package:driven_site_locator/site_locator/modules/left_header/web_menu/web_menu_row.dart';
 import 'package:driven_site_locator/site_locator/styles/site_locator_colors.dart';
+import 'package:driven_site_locator/site_locator/utilities/site_locator_utils.dart';
 import 'package:flutter/material.dart';
 
 class WebHelpCenterCard extends StatelessWidget {
@@ -17,12 +17,10 @@ class WebHelpCenterCard extends StatelessWidget {
     );
   }
 
-  void navToHelpCenterPage() {
-    SiteLocatorNavigation.instance.toCommonWebView(
-      url: SiteLocatorConfig.helpCenterUrl,
-      title: SiteLocatorConstants.helpCenter,
-    );
-  }
+  void navToHelpCenterPage() => SiteLocatorUtils.launchURL(
+        SiteLocatorConfig.helpCenterUrl,
+        SiteLocatorConstants.openApplyForFuelmanError,
+      );
 
   Icon _helpCenterIcon() => const Icon(
         Icons.help_center_outlined,
--- lib/site_locator/modules/left_header/web_menu/web_menu_items/web_legal_privacy_card.dart
+++ lib/site_locator/modules/left_header/web_menu/web_menu_items/web_legal_privacy_card.dart
@@ -1,10 +1,9 @@
-import 'package:driven_site_locator/config/site_locator_navigation.dart';
 import 'package:driven_site_locator/constants/api_constants.dart';
-import 'package:driven_site_locator/constants/app_strings.dart';
 import 'package:driven_site_locator/driven_components/driven_components.dart';
 import 'package:driven_site_locator/site_locator/constants/site_locator_constants.dart';
 import 'package:driven_site_locator/site_locator/modules/left_header/web_menu/web_menu_row.dart';
 import 'package:driven_site_locator/site_locator/styles/site_locator_colors.dart';
+import 'package:driven_site_locator/site_locator/utilities/site_locator_utils.dart';
 
 class WebLegalPrivacyCard extends StatelessWidget {
   const WebLegalPrivacyCard({super.key});
@@ -18,12 +17,10 @@ class WebLegalPrivacyCard extends StatelessWidget {
     );
   }
 
-  void navToLegalPage() {
-    SiteLocatorNavigation.instance.toCommonWebView(
-      url: ApiConstants.fuelmanLegalUrl,
-      title: AppStrings.fuelmanLegalPrivacy,
-    );
-  }
+  void navToLegalPage() => SiteLocatorUtils.launchURL(
+        ApiConstants.fuelmanLegalUrl,
+        SiteLocatorConstants.openApplyForFuelmanError,
+      );
 
   Icon _legalPrivacyIcon() => const Icon(
         Icons.policy_outlined,
