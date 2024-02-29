dfc4fccd6f5a8a107e25aff731de8fac639db716
SnehaMudhigonda <sneha.mudhigonda@fleetcor.com>
2024-03-01 01:57:15 +0530

    Reset list

--- lib/site_locator/modules/filters/widgets/button/quick_filters_list.dart
+++ lib/site_locator/modules/filters/widgets/button/quick_filters_list.dart
@@ -38,7 +38,6 @@ class QuickFiltersList extends StatelessWidget {
       siteLocatorController.canShow2CTA(false);
       siteLocatorController.clearSearchPlaceInput();
       await filterController.syncEnhancedFilter(siteFilter);
-      await siteLocatorController.setListViewInitializers();
     }
   }
 
