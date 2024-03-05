50854c10d8885b1e680feaff74272e0ca7355041
SivaGandrikala <siva.gandrikala@fleetcor.com>
2024-03-05 16:18:41 +0530

    Revert map pin style back to f13

--- lib/site_locator/site_locator_map/core/custom_pin_markers/custom_pin.dart
+++ lib/site_locator/site_locator_map/core/custom_pin_markers/custom_pin.dart
@@ -470,7 +470,7 @@ class CustomPin {
     textPainter.text = TextSpan(
         text: text,
         style: kIsWeb
-            ? f14BoldWhite.copyWith(fontWeight: FontWeight.w600)
+            ? f13BoldWhite.copyWith(fontWeight: FontWeight.w600)
             : f26BoldWhite.copyWith(fontWeight: FontWeight.w600));
 
     textPainter.layout();
