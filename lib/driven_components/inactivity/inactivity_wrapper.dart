d26c83ce8cfb587f64c504fcf995906b0648f57e
hitesh.danidhariya <hitesh.danidhariya@fleetcor.com>
2025-06-24 17:49:17 +0530

    Put controller

--- lib/driven_components/inactivity/inactivity_wrapper.dart
+++ lib/driven_components/inactivity/inactivity_wrapper.dart
@@ -2,7 +2,7 @@
 part of inactivity_module;
 
 class SLInactivityWrapper extends StatelessWidget {
-  final SLInactivityController controller = Get.find();
+  final SLInactivityController controller = Get.put(SLInactivityController());
 
   final Widget? child;
   VoidCallback? sessionExpiryWarningTimerCallback;
