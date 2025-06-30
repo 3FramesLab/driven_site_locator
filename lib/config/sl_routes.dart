f4fbe806fd23028e72e51e2d53cd9b47cf118ce2
Smeet Bhat <smeet.bhat@corpay.com>
2025-06-30 17:45:22 +0530

    Update sl_routes.dart

--- lib/config/sl_routes.dart
+++ lib/config/sl_routes.dart
@@ -1,5 +1,5 @@
 class SLRoutes {
-  static const searchPlaceResultsView = '/searchPlaceResultsView';
+  static const searchPlaceResultsView = '/slSearchPlaceResultsView';
   static const String searchPlace = '/searchPlace';
   static const String unauthSiteLocator = '/unauthSiteLocatorMap';
 }
